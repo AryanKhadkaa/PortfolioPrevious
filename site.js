0c923ae77a9ad52a5841be2b158809ab38d958c7
AryanKhadkaa <121329367+AryanKhadkaa@users.noreply.github.com>
2023-01-22 15:01:39 +0545

    Add files via upload

---
+++ site.js
@@ -0,0 +1,19 @@
+
+function theme(){
+    x= document.body;
+    x.classList.toggle("darkmode");
+}
+
+function display(){
+    document.getElementById('sidebar').classList.toggle('active');
+}
+
+/* function empty(){
+    document.getElementById("tbname").value="";
+    document.getElementById("t2").value="";
+    document.getElementById("t3").value="";
+    document.getElementById("t4").value="";
+
+}
+*/
+
