11ace86795ffc9941921eace99eac407c1b6d3ef
AryanKhadkaa <121329367+AryanKhadkaa@users.noreply.github.com>
2023-01-25 23:26:36 +0545

    Update site.js

--- site.js
+++ site.js
@@ -4,11 +4,33 @@ function theme(){
     x.classList.toggle("darkmode");
 }
 
+
+const bar = document.querySelector("#info-bar");
+const pscroll = window.pageYOffset;
+
+             console.log(pscroll)
+
+window.onscroll= function() {
+    const nscroll = window.pageYOffset;
+
+                                console.log(nscroll);
+if(nscroll > pscroll)
+{                               console.log("working")
+    bar.style.top = "-60px";
+}
+else{
+    bar.style.top = "0px";
+}
+   pscroll=nscroll;
+}
+
+
 function display(){
     document.getElementById('sidebar').classList.toggle('active');
 }
 
-/* function empty(){
+/*
+ function empty(){
     document.getElementById("tbname").value="";
     document.getElementById("t2").value="";
     document.getElementById("t3").value="";
@@ -17,3 +39,8 @@ function display(){
 }
 */
 
+
+
+
+
+        
